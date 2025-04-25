4c1a6aa507117125b55a7538717fa9855412f195
ganeshkr-54 <ganeshkumarreddy54@gmail.com>
2025-04-25 23:00:57 +0530

    vpc output file & ec2 bashion variables

--- vpc-module.tf
+++ vpc-module.tf
@@ -11,6 +11,29 @@ module "vpc" {
   azs                 = data.aws_availability_zones.available.name
   private_subnets     = var.vpc_private_subnets
   public_subnets      = var.vpc_public_subnets
-  database_subnets    = var.vpc_database_subnets
 
+  # Database componentes
+  database_subnets    = var.vpc_database_subnets
+  create_database_subnet_group = var.vpc_create_database_subnet_group
+  create_database_subnet_route_table = var.vpc_create_database_subnet_route_table
+
+  # Nat Gateway for outbound connection
+  enable_nat_gateway = var.vpc_enable_nat_gateway
+  single_nat_gateway = var.vpc_single_nat_gateway
+
+# VPC DNS Parameters
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+# Tags
+
+vpc_tags = local.common_tags
+
+ # Additional Tags to Subnets
+  public_subnet_tags = {
+    Type = "Public Subnets"
+    "kubernetes.io/role/elb" = 1    
+    "kubernetes.io/cluster/${local.eks_cluster_name}" = "shared"        
+  }
+  map_public_ip_on_launch = true
 }
