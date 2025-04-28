cdd1cc0ef329f534f6ff08eabf7220ceb4032810
ganeshkr-54 <ganeshkumarreddy54@gmail.com>
2025-04-28 15:20:53 +0530

    Eks Cluster Creation

--- bigfiles.sh
+++
@@ -1,7 +0,0 @@
-git rev-list --objects --all | sort -k 2 | while read hash name; do
-  size=$(git cat-file -s $hash)
-  if [ $size -gt 100000000 ]; then
-    echo "$size bytes : $name"
-  fi
-done
-
--- ec2-bastion-eip.tf
+++
@@ -1,9 +0,0 @@
-# Create Elastic IP for Bastion Host
-
-resource "aws_eip" "bastion_eip" {
-    instance = module.ec2-public.id
-    depends_on = [ module.ec2-public, module.vpc ]
-    domain = "vpc"
-    tags = local.common_tags
-  
-}
