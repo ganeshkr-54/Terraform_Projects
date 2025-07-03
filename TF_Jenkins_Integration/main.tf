0bfbb6e70699dde6160437f58baff21cc61b5d5f
Ganesh Kumar Reddy <158801596+ganeshkr-54@users.noreply.github.com>
2025-07-03 17:58:12 +0530

    Update main.tf

--- TF_Jenkins_Integration/main.tf
+++ TF_Jenkins_Integration/main.tf
@@ -7,11 +7,3 @@ resource "aws_instance" "my-vm" {
 
 }
 
-resource "aws_instance" "my_vm" {
- ami           = var.ami //Ubuntu AMI
- instance_type = var.instance_type
-
- tags = {
-   Name = var.name_tag,
- }
-}
